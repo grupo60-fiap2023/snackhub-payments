775d011412c48218236bd774b9c6d1deb27ed640
MarlonFC <marlonfcosta90@gmail.com>
2023-11-11 01:58:49 -0300

    Create project

---
+++ src/main/java/com/alura/fiap/snackhubpayments/SnackhubPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.alura.fiap.snackhubpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackhubPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackhubPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/fiap/snackhubpayments/SnackhubPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.fiap.snackhubpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackhubPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
